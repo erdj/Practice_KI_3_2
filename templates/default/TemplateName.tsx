55f52951310ae22b26f48b93403d8a379675c95f
Dima <62839782+Dimutriy-web@users.noreply.github.com>
2023-05-28 21:02:55 +0300

    Delete templates/default directory

--- templates/default/TemplateName.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './TemplateName.module.css';
-
-// interface TemplateNameProps {}
-// {}: TemplateNameProps
-
-export const TemplateName = () => {
-  return <div className={styles.templateName}>TemplateName Component</div>;
-};
